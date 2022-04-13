d50e45cfff4069d34a5940efcdfa6ef862c0c198
조준희 <zzuni3423@naver.com>
2022-04-13 16:39:31 +0900

    [ADD] 파일 재생성

--- SejongTimeTable/Constants.cs
+++ SejongTimeTable/Constants.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace SejongTimeTable
 {
-    internal class Constant
+    internal class Constants
     {
     }
 }
