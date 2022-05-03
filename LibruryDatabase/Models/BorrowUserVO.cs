96870eefb4a5e5804d481bd9aa399e8addf9e035
조준희 <zzuni3423@naver.com>
2022-05-03 11:27:05 +0900

    [ADD] 제출 전 저장

--- LibruryDatabase/Models/BorrowUserVO.cs
+++ LibruryDatabase/Models/BorrowUserVO.cs
@@ -8,5 +8,32 @@ namespace LibruryDatabase.Models
 {
     internal class BorrowUserVO
     {
+        public string id;
+        public string number;
+        public string title;
+        public string author;
+        public string publish;
+        public string borrowbook;
+        public string returnbook;
+
+
+        public BorrowUserVO(string id, string number, string title, string author, string publish, string borrowbook, string returnbook)
+        {
+            this.id = id;
+            this.number = number;
+            this.title = title;
+            this.author = author;
+            this.publish = publish;
+            this.borrowbook = borrowbook;
+            this.returnbook = returnbook;
+        }
+
+        public BorrowUserVO()
+        {
+
+        }
+
+        
+
     }
 }
