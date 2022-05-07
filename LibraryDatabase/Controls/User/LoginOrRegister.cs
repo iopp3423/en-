5cb93d40d4e22cba0d0dcb443e09d85dfe3ddee2
조준희 <zzuni3423@naver.com>
2022-05-07 10:01:40 +0900

    [ADD] 변경사항저장

--- LibraryDatabase/Controls/User/LoginOrRegister.cs
+++ LibraryDatabase/Controls/User/LoginOrRegister.cs
@@ -282,9 +282,8 @@ namespace LibruryDatabase.Controls
             while (Constants.isLogin)
             {
                 Console.SetCursorPosition(Constants.ID_X, Constants.ID_Y);
-                //id = Console.ReadLine();
-                id = Input();
-                if (id == "#") return "#";
+                id = Console.ReadLine();
+
 
                 if (CheckNull(id))
                 {
@@ -545,42 +544,6 @@ namespace LibruryDatabase.Controls
                 return Constants.isPassing;
             }
             return Constants.isFail;
-        }
-
-        public string Input()
-        {
-            string input = "";
-            ConsoleKeyInfo info = Console.ReadKey(true);
-            while (info.Key != ConsoleKey.Enter)
-            {
-                if (Constants.isPassing != Regex.IsMatch(info.KeyChar.ToString(), Utility.Exception.INPUT))
-                {
-                    Console.Write(info.KeyChar.ToString());
-                    input += info.KeyChar;
-                }
-                else if (info.Key == ConsoleKey.Backspace)
-                {
-                    if (!string.IsNullOrEmpty(input))
-                    {
-                        input = input.Substring(Constants.CURRENT_LOCATION, input.Length - Constants.ONE);
-                        int passwordX = Console.CursorLeft;
-                        Console.SetCursorPosition(passwordX - Constants.ONE, Console.CursorTop);
-                        Console.Write(" ");
-                        Console.SetCursorPosition(passwordX - Constants.ONE, Console.CursorTop);
-                    }
-                }
-                else if (info.Key == ConsoleKey.Escape)
-                {
-                    Console.Clear();
-                    Menu.PrintMain();
-                    Menu.RegisterOrLogin();
-                    return "#";
-                }
-                info = Console.ReadKey(true);
-
-            }
-            Console.WriteLine();
-            return input;
         }
     }
 
--- LibraryDatabase/Open.cs
+++ LibraryDatabase/Open.cs
@@ -11,12 +11,39 @@ namespace LibruryDatabase
 {
     internal class Open
     {
-
         static void Main(string[] args)
         {
             ChoiceUserOrAdmin Start = new ChoiceUserOrAdmin();
-            Start.StartMenu();
+            //Start.StartMenu();
             //관리자 id = enen1234, pw = enen4321
+
+
+
+            string input = "";
+            ConsoleKeyInfo info = Console.ReadKey();
+            while (info.Key != ConsoleKey.Enter)
+            {
+                if (info.Key != ConsoleKey.Backspace)
+                {
+                    input += info.KeyChar;
+                }
+                else if (info.Key == ConsoleKey.Backspace)
+                {
+                    if (!string.IsNullOrEmpty(input))
+                    {
+                        input = input.Substring(Constants.CURRENT_LOCATION, input.Length);
+                        int passwordX = Console.CursorLeft;
+                        Console.SetCursorPosition(passwordX , Console.CursorTop);
+                        Console.Write(" ");
+                        Console.SetCursorPosition(passwordX, Console.CursorTop);
+                    }
+                }
+                info = Console.ReadKey();
+            }
+            Console.WriteLine();
+            Console.WriteLine(input);
+
+
         }       
     }
 }
