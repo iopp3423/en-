42d0a2fc2a81c2d0d6332dfbf9521dfd1331c150
조준희 <iopp3423@gmail.com>
2022-05-09 16:03:06 +0900

    [FIX] 패널오류 수정

--- EnProject/ProjectImg/Open/src/use/OpenProject.java
+++ EnProject/ProjectImg/Open/src/use/OpenProject.java
@@ -1,12 +1,11 @@
 package use;
 
 
-public class OpenProject {
+public class OpenProject{
 
 	public static void main(String[] args) {
 		Screen screen = new Screen();
-		screen.frame.setVisible(true);
-		
+		screen.frame.setVisible(true);		
 		//Database db = new Database();
 		//db.db();
 	}
--- EnProject/ProjectImg/Open/src/use/Screen.java
+++ EnProject/ProjectImg/Open/src/use/Screen.java
@@ -1,20 +1,23 @@
 package use;
 
-import java.awt.*;
-import javax.swing.*;
+
 import java.awt.Dimension;
-import java.awt.Graphics;
 import java.awt.GridLayout;
-import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemListener;
 import java.awt.event.ItemEvent;
 
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 
 public class Screen {
 	
-	JFrame frame;
+	public JFrame frame;
 
 	
 	public Screen() {
@@ -23,87 +26,59 @@ public class Screen {
 	
 	public void initialize()
 	{
-		frame = new JFrame(); // 프레임 생성 
-		frame.setResizable(false); // 창 크기 조절 불가
-		//frame.setVisible(true); //창 뜨게하기 
-		frame.setPreferredSize(new Dimension(840, 840/12*9));
+		
+		frame = new JFrame(); // 프레임 생성
+
 		frame.setSize(840, 840/12*9);
+		frame.setResizable(false); // 창 크기 조절 불가
+		frame.setPreferredSize(new Dimension(840, 840/12*9));	
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 프로그램 실행 후 종료
-		frame.setLocationRelativeTo(null); // 화면 나오는 위치  
+		frame.setLocationRelativeTo(null); // 화면 나오는 위치
+		
 		
 		JPanel FirstPanel = new JPanel();
 		JPanel SearchPanel = new JPanel();
 		JPanel RecordPanel = new JPanel();
+		FirstPanel.setLayout(null);
+		SearchPanel.setLayout(null);
 		
-		frame.getContentPane().add(FirstPanel);
-		
-		JButton search = new JButton("검색하기");
-		JButton record = new JButton("기록보기");		
-		JTextField Input = new JTextField(10);
-		
-		JPanel panel = new JPanel();
-		JPanel button = new JPanel();
-		
-		search.setBounds(300, 123, 100, 40); // 버튼위치
-		record.setBounds(60, 123, 100, 40); // 버튼위치 수
-		Input.setBounds(90, 123, 259, 40); // 버튼위치 수
-		
-		FirstPanel.add(search);
-		FirstPanel.add(record);
-		FirstPanel.add(Input);
-		
-		FirstPanel.setVisible(true);
-		
-		
-		
-		search.addActionListener(new ActionListener(){ // 검색하기 
-			public void actionPerformed(ActionEvent e) {
 
-				frame.setVisible(false);
-				SearchingImage(); // 검색하기 
-				
-			}
-				
-		});
-		
-		record.addActionListener(new ActionListener(){ // 기록보기 
-			public void actionPerformed(ActionEvent e) {
+		frame.getContentPane().add(FirstPanel);	
 
-				frame.setVisible(false);
-				CheckLog(); // 기록보기 
-			}
+		
+		//FirstPanel.setVisible(false);
+		//SearchPanel.setVisible(true);
+		
+		//frame.getContentPane().add(RecordPanel);		
 				
-		});
 		
-		
-	
-	}
-
-	
-	public void SearchingImage() // 검색하기 
-	{
-		JFrame frame = new JFrame(); // 프레임 생성 
+		///////////////////////////////////////검색 패널 
 		String[] Choose = {"10", "20","30"};
-		JButton search = new JButton("검색하기");
+		JButton Secondsearch = new JButton("검색하기");
 		JButton back = new JButton("뒤로가기");
-		JPanel button = new JPanel();
-		JPanel panel = new JPanel();
+		JTextField SecondInput = new JTextField(10);
+		JComboBox comboBox = new JComboBox(Choose);
+		
+		Secondsearch.setBounds(500, 50, 100, 40); // 버튼위치
+		back.setBounds(600, 50, 100, 40); // 
+		SecondInput.setBounds(200,50, 300, 40); // 버튼위치 수d
+		comboBox.setBounds(700,50, 70, 40);
 		JPanel gridOne = new JPanel(new GridLayout(2,5));
 		JPanel gridTwo = new JPanel(new GridLayout(4,5));
-		JPanel gridThree = new JPanel(new GridLayout(6,5));
+		JPanel gridThree = new JPanel(new GridLayout(6,5));	
+		gridOne.setLayout(null);
+		gridOne.setLocation(100,100);
 		
-		JTextField Input = new JTextField(10);
-		JComboBox comboBox = new JComboBox(Choose);
 		
 		String name[] = {"컴포턴트 1","컴포턴트 1","컴포턴트 1","컴포턴트 1","컴포턴트 1","컴포턴트 1","컴포턴트 1","컴포턴트 1","컴포턴트 1","컴포턴트 1"};
 			
-		button.add(search);
-		button.add(back);
-		panel.add(Input);
-		panel.add(button);
-		panel.add(comboBox);
 		
-		System.out.println(comboBox.getItemAt(comboBox.getSelectedIndex()));
+
+		SearchPanel.add(SecondInput); // 입력창 
+		SearchPanel.add(Secondsearch); // 검색버튼 
+		SearchPanel.add(back);// 뒤로가기 버튼 
+		SearchPanel.add(comboBox);
+		SearchPanel.add(gridOne);
 		
 			
 		comboBox.addItemListener(new ItemListener() {
@@ -124,10 +99,11 @@ public class Screen {
 		{ 	for(int i=0;i<10;i++)
 			{
 				JButton jb = new JButton(name[i]);
-				gridOne.setSize(300, 200);
+				gridOne.setSize(200, 200);
 				gridOne.add(jb);
 			}
 		}
+		
 		if(comboBox.getItemAt(comboBox.getSelectedIndex()) == "20")
 		{ 	for(int i=0;i<20;i++)
 			{
@@ -144,69 +120,93 @@ public class Screen {
 				gridThree.add(jb);
 			}
 		}
-		
-		panel.add(gridOne);
-		frame.add(panel);
-		
+				
 		
 		
 		back.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e) {
 				
-				frame.setVisible(false);
-				Screen screen = new Screen();
+				SearchPanel.setVisible(false);
+				frame.getContentPane().removeAll();
+				frame.getContentPane().add(FirstPanel);	
+				FirstPanel.setVisible(true);
 			}
 				
 		});
 		
-		frame.setResizable(false); // 창 크기 조절 불가
-		frame.setVisible(true); //창 뜨게하기 
-		frame.setPreferredSize(new Dimension(840, 840/12*9));
-		frame.setSize(840, 840/12*9);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 프로그램 실행 후 종료
-		frame.setLocationRelativeTo(null); // 화면 나오는 위치  
+		
+		JButton Firstsearch = new JButton("검색하기");
+		JButton record = new JButton("기록보기");		
+		JTextField Input = new JTextField(10);
 		
 		
-	}
-	
-	
-	
-	public void CheckLog() // 기록보기  
-	{	
-		JFrame frame = new JFrame(); // 프레임 생성 
+		Firstsearch.setBounds(500, 50, 100, 40); // 버튼위치
+		record.setBounds(600, 50, 100, 40); // 
+		Input.setBounds(200, 50, 300, 40); // 버튼위치 수d
 		
+		FirstPanel.add(Input);
+		FirstPanel.add(Firstsearch);
+		FirstPanel.add(record);
+		
+		
+		//SearchPanel.setVisible(true);
+		//FirstPanel.setVisible(true);
+	
+		
+		Firstsearch.addActionListener(new ActionListener(){ // 검색하기 
+			public void actionPerformed(ActionEvent e) {
+
+				FirstPanel.setVisible(false);
+				frame.getContentPane().removeAll();
+				frame.getContentPane().add(SearchPanel);
+				SearchPanel.setVisible(true);
+				//System.out.println("Hello world");
+			}
+				
+		});
+		
+		record.addActionListener(new ActionListener(){ // 기록보기 
+			public void actionPerformed(ActionEvent e) {
+
+				FirstPanel.setVisible(false);
+				frame.getContentPane().removeAll();
+				frame.getContentPane().add(RecordPanel);
+				RecordPanel.setVisible(true);
+			}
+				
+		});
+		
+	
+	
+		///// 기록 보기
+		/*
 		JButton search = new JButton("검색하기");
 		JButton back = new JButton("뒤로가기");
 		JPanel button = new JPanel();
 		JPanel panel = new JPanel();
 		JLabel title = new JLabel("로그 기록");
 		title.setSize(150, 150);
+		*/
 		//JTextField Input = new JTextField(10);
 		
 		
 		
-		button.add(search);
-		button.add(back);
-		panel.add(title);
-		panel.add(button);
+		//button.add(search);
+		//button.add(back);
+		//panel.add(title);
+		//panel.add(button);
 		
-		frame.add(panel);
+		//frame.add(panel);
 		
 		
-		back.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e) {
+		//back.addActionListener(new ActionListener(){
+			//public void actionPerformed(ActionEvent e) {
 				
 				//frame.setVisible(false);
-			}
+			//}
 				
-		});
+		//});
 		
-		frame.setResizable(false); // 창 크기 조절 불가
-		frame.setVisible(true); //창 뜨게하기 
-		frame.setPreferredSize(new Dimension(840, 840/12*9));
-		frame.setSize(840, 840/12*9);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 프로그램 실행 후 종료
-		frame.setLocationRelativeTo(null); // 화면 나오는 위치  
 		
 	}
 	
@@ -215,14 +215,9 @@ public class Screen {
 	{
 		image.drawImage(image, 0, 0, null);
 	}
+	
+	
+	
 	*/
-	
-	
-	
-	
-	public void AdjustFrame()
-	{
-	
-	}
 	
 }
