9eb25cd51bad412f3b1861e4fa10fee36ccd3742
조준희 <iopp3423@gmail.com>
2022-05-09 22:48:10 +0900

    [FIX] json 파싱 오류 해결..

--- EnProject/ProjectImg/Open/src/use/test.java
+++ EnProject/ProjectImg/Open/src/use/test.java
@@ -1,5 +1,8 @@
 package use;
 
+
+import java.io.*;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
@@ -8,122 +11,22 @@ import java.net.URL;
 import java.io.DataOutputStream;
 import java.net.URLEncoder;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+
+
 public class test {
 
-	public void api()
-	{
-		try {
-		String apikey = "{26a41b7bfe87ed292acb3bbb3f064df6}";	    
-		
-		String apiURL = "https://dapi.kakao.com/v2/search/image.json?query=설현";
-		URL url = new URL(apiURL);
-		HttpURLConnection con = (HttpURLConnection)url.openConnection();
-		String userCredentials = apikey;
-		String basicAuth = "KakaoAK" + userCredentials;
-		con.setRequestMethod("GET");
-		con.setRequestProperty("Authorization:",basicAuth);
-		con.setRequestProperty("Content-type", "application/json");
-		con.setUseCaches(false);
-		con.setDoInput(true);
-		con.setDoOutput(true);
-		int responseCode = con.getResponseCode();
-		System.out.println("responseCode >> " + responseCode);
-		BufferedReader br;
-		if(responseCode == 200) {
-			br = new BufferedReader(new InputStreamReader(con.getInputStream()));
-		}
-		else {
-			br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
-		}
-			String inputLine;
-			StringBuffer res = new StringBuffer();
-			while((inputLine = br.readLine()) != null)
-			{
-				res.append(inputLine);
-			}
-			br.close();
-			System.out.println("응답 결과 >>  "+res.toString());
-		}
-		catch(Exception e)
-		{
-			System.out.println("오류발생");
-		}
-		
-		
-	
-	}
-//	/{26a41b7bfe87ed292acb3bbb3f064df6}
-	
-	public void apiTestGet() throws Exception 
-	{
-	    URL url = null;
-	    String readLine = null;
-	    StringBuilder buffer = null;
-	    BufferedReader bufferedReader = null;
-	    BufferedWriter bufferedWriter = null;
-	    HttpURLConnection urlConnection = null;
-	    	
-	    int connTimeout = 5000;
-	    int readTimeout = 3000;
-			
-	    String apiUrl = "https://dapi.kakao.com/v2/search/image?query=설현";	// 각자 상황에 맞는 IP & url 사용 		
-			
-	    try 
-	    {
-	        url = new URL(apiUrl);
-	        
-	        urlConnection = (HttpURLConnection)url.openConnection();
-	        urlConnection.setRequestMethod("GET"); // GET 있었
-	        urlConnection.setConnectTimeout(connTimeout);
-	        urlConnection.setReadTimeout(readTimeout);
-	        urlConnection.setRequestProperty("Accept", "application/json;");
-				
-	        buffer = new StringBuilder();
-	        
-	        if(urlConnection.getResponseCode() == HttpURLConnection.HTTP_OK) 
-	        {
-	            bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(),"UTF-8"));
-	            while((readLine = bufferedReader.readLine()) != null) 
-	    	    {
-	                buffer.append(readLine).append("\n");
-	            }
-	        }
-	        else
-	        {
-	            buffer.append("code : ");
-	            buffer.append(urlConnection.getResponseCode()).append("\n");
-	            buffer.append("message : ");
-	            buffer.append(urlConnection.getResponseMessage()).append("\n");
-	        }
-	    }
-	    catch(Exception ex) 
-	    {
-	        ex.printStackTrace();
-	    }
-	    finally 
-	    {
-	        try 
-	        {
-	            if (bufferedWriter != null) { bufferedWriter.close(); }
-	            if (bufferedReader != null) { bufferedReader.close(); }
-	        }
-	        catch(Exception ex) 
-	        { 
-	            ex.printStackTrace();
-	        }
-	    }
-			
-			
-	        System.out.println("결과 : " + buffer.toString());
-	    }
-	
-	static final String BASE_URL = "https://dapi.kakao.com/v2/search/image?sort=accuracy&page=1&size=10&query=설현";
+	static final String BASE_URL = "https://dapi.kakao.com/v2/search/image?sort=accuracy&page=1&size=1&query=설현";
 	//"https://dapi.kakao.com/v2/search/image?sort=accuracy&page=1&size="+comboboxNumber+"&query="설현 "
 	//static final String AUTH_TOKEN = "AUTH_TOKEN 값";
 	//static String AUTH_KEY = "Bearer " + "AUTH_KEY 값";
 	public void testt()
 	{
-
+		JSONObject result = null;
 		/**
 		 *  REST API 호출하기
 		 */
@@ -152,15 +55,44 @@ public class test {
 			while(br.ready()) {
 				sb.append(br.readLine());	
 				System.out.println(sb);
-				System.out.println("\n");
+				
 			}
-			
+		
 			con.disconnect();
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
-
+		
+				
+			try {
+				result = (JSONObject) new JSONParser().parse(sb.toString());
+				System.out.println(result);
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			
-	}
-	    
+			StringBuilder out = new StringBuilder();
+			out.append(result.get("status") +" : " + result.get("status_message") +"\n");
+				
+	
+			
+			// JSONObject에서 Array데이터를 get하여 JSONArray에 저장한다.
+			JSONArray array = (JSONArray) result.get("documents");
+			//System.out.println(array);
+			JSONObject arr = (JSONObject)array.get(0);
+			
+			System.out.println(arr.get("image_url"));
+			out.append("데이터 출력하기 \n");
+			//for(int i=0; i<array.size(); i++) {
+			//	tmp = (JSONObject) array.get(i);
+			//	out.append("title("+i+") :"+ tmp.get("image_url") +"\n");
+			//	
+			//	out.append("\n");
+			//}
+
+	
+	
+	}	    
+	
 }
