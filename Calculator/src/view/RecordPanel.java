00e46c5161796f3cb2e07982df81c3609c6726ba
조준희 <iopp3423@gmail.com>
2022-05-22 15:07:18 +0900

    [ADD] 로그 글씨크기 확대 코드 추가

--- Calculator/src/view/RecordPanel.java
+++ Calculator/src/view/RecordPanel.java
@@ -3,6 +3,7 @@ package view;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -36,6 +37,7 @@ public class RecordPanel extends JPanel{
 			button[index].setOpaque(true);//있어야 색 적용가능
 			button[index].setBorderPainted(false);//있어야 색 적용가능 
 			button[index].setMaximumSize(new Dimension(200,300));
+			//button[index].setFont(new Font("맑은 고딕", Constants.RESET, Constants.FONT_SIZE));
 			if(index == Constants.RESET) button[index].setText("아직 기록이 없습니다.");
 			add(button[index]);
 			button[index].addActionListener(printRecord);
